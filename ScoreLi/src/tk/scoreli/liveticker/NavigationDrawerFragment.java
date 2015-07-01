0919a48cc21680510fe7e161a39cd6eef02c0f4e
Philipp Messmer <philipp.messmer@gmail.com>
2015-07-01 17:34:35 +0200

    Manifest berreinigt und sonstige unnötige Strings

--- ScoreLi/src/tk/scoreli/liveticker/NavigationDrawerFragment.java
+++ ScoreLi/src/tk/scoreli/liveticker/NavigationDrawerFragment.java
@@ -1,6 +1,5 @@
 package tk.scoreli.liveticker;
 
-import tk.scoreli.liveticker.data.DatabasehandlerSpiele;
 import tk.scoreli.liveticker.internet.InternetService;
 import android.app.ActionBar;
 import android.app.Activity;
@@ -13,15 +12,12 @@ import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 /**
  * Fragment used for managing interactions for and presentation of a navigation
@@ -108,8 +104,8 @@ public class NavigationDrawerFragment extends Fragment {
 					}
 				});
 		/**
-		 * Hier kann man die Einzelen Section von der Navigation Bar hinzufuegen.
-		 * Diese muss aber auch dann als String in res vorliegen.
+		 * Hier kann man die Einzelen Section von der Navigation Bar
+		 * hinzufuegen. Diese muss aber auch dann als String in res vorliegen.
 		 */
 		mDrawerListView.setAdapter(new ArrayAdapter<String>(getActionBar()
 				.getThemedContext(),
@@ -262,19 +258,6 @@ public class NavigationDrawerFragment extends Fragment {
 	}
 
 	@Override
-	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-		// If the drawer is open, show the global app actions in the action bar.
-		// See also
-		// showGlobalContextActionBar, which controls the top-left area of the
-		// action bar.
-		if (mDrawerLayout != null && isDrawerOpen()) {
-			inflater.inflate(R.menu.global, menu);
-			showGlobalContextActionBar();
-		}
-		super.onCreateOptionsMenu(menu, inflater);
-	}
-
-	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		if (mDrawerToggle.onOptionsItemSelected(item)) {
 			return true;
@@ -285,7 +268,7 @@ public class NavigationDrawerFragment extends Fragment {
 		 */
 		if (item.getItemId() == R.id.action_example) {
 			internetService.Veranstaltungholen();
-					
+
 			return true;
 		}
 
